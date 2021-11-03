97dbca94f3d686c99b32d5c4c9a2791aabfb106a
salamandaaa <hht2010ily@hotmail.com>
2021-11-03 10:03:59 +0800

    deploy

--- migrations/2_initial_CryptoKitty.js
+++ migrations/2_initial_CryptoKitty.js
@@ -1,4 +1,20 @@
-const CryptoKitty = artifacts.require("CryptoKitty"); //需要部署的合约名称 
+const KittyCore = artifacts.require("KittyCore");
+const GeneSimple = artifacts.require("GeneSimple");
+const SaleClockAuction = artifacts.require("SaleClockAuction");
+const SiringClockAuction = artifacts.require("SiringClockAuction");
+
+const CUT = 5000;
+
 module.exports = function(deployer) {
-      deployer.deploy(CryptoKitty);
+      await deployer.deploy(KittyCore);
+      console.log("KittyCore address: ", KittyCore.address);
+
+      await deployer.deploy(GeneSimple);
+      console.log("GeneSimple address: ", GeneSimple.address);
+
+      await deployer.deploy(SaleClockAuction, KittyCore.address, CUT);
+      console.log("SaleClockAuction address: ", SaleClockAuction.address);
+
+      await deployer.deploy(SiringClockAuction, KittyCore.address, CUT);
+      console.log("SiringClockAuction address: ", SiringClockAuction.address);
 };
--- truffle-config.js
+++ truffle-config.js
@@ -34,7 +34,7 @@ module.exports = {
     // options below to some value.
     //
     development: {
-     host: "127.0.0.1",     // Localhost (default: none)
+     host: "192.168.1.49",     // Localhost (default: none)
      port: 6789,            // Standard Ethereum port (default: none)
      network_id: "*",       // Any network (default: none)
      from: "lat1k422xnhua76asazwvref57mm6454r5gamtvdgd",        // Account to send txs from (default: accounts[0])
